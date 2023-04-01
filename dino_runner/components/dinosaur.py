c6da37842f8296d1b1c38372b3168ef44ecee7d6
Karem Huacota <karem.huacota@gmail.com>
2023-03-31 20:24:28 -0400

    Clase_1

---
+++ dino_runner/components/dinosaur.py
@@ -0,0 +1,14 @@
+from dino_runner.utils.constants import RUNNING, DUCKING, JUMPING
+
+class Dinosaur:
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+    def duck(self):
+        pass
+
+    def jump(self):
+        pass

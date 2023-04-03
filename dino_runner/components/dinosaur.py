011ad1d77e9a43b01a9e9fe47f05b9eb0dd70857
YoshuaHR <99432055+YoshuaHR@users.noreply.github.com>
2023-04-02 21:02:51 -0500

    Problemas 1

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,14 +1,43 @@
-from dino_runner.utils.constants import RUNNING, DUCKING, JUMPING
+import pygame
+from dino_runner.utils.constants import RUNNING,DUCKING,JUMPING
+from pygame.sprite import Sprite
 
-class Dinosaur:
-    def __init__(self):
-        pass
+class Dinosaur(Sprite):
+    X_POS = 80
+    Y_POS = 310
+    POS_Y_DUCKING = 345
+    JUMP_VEL = 10
 
-    def run(self):
-        pass
+def __init__(self):
+        self.image = RUNNING[0]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.POS_X
+        self.dino_rect.y = self.POS_Y
+        self.step_index = 0
+        self.dino_run = True
+        self.dino_duck = False
+        self.dino_jump = False
 
-    def duck(self):
-        pass
+def update(self, user_input):
+    if user_input[pygame.K_DOWN]:
+        self.duck()
+    else:
+         self.run()
+    self.run()
+    if self.step_index >= 10:
+        self.step_index = 0
 
-    def jump(self):
-        pass
+def draw(self, screen):
+    screen.blit(self.image, self.dino_rect)
+    
+def run(self):
+    self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
+    self.dino_rect.x = self.POS_X
+    self.dino_rect.y = self.POS_Y
+    self.step_index += 1
+    
+def duck(self):
+    pass
+
+def jump(self):
+     pass
